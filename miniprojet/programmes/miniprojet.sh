d475501ceb1fd1d17ee667e262e8f64f979adba6
lena <lena.brun@orange.fr>
2024-11-18 17:58:03 +0100

    Mini projet 2

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -8,11 +8,33 @@ then
     echo "Ce programme demande un argument : un fichier."
     exit
 fi
+echo -e "<html>
+	<head>
+		<meta charset="UTF-8" />
+	</head>
+	<body>
+		<table>
+			<tr>
+				<th>Ligne</th>
+				<th>Lien</th>
+				<th>Code HTTP</th>
+				<th>Encodage</th>
+				<th>Mots</th>
+			</tr>"
 
 while read -r line
 do
-    http_code=$(curl -o /dev/null -s -w '%{http_code}' "$line")
-    encoding=$(curl -sI "$line" | grep -i "Content-Type" | awk -F 'charset'= '{print $2}' | tr -d '\r' | tr -d '\n')
-    words_count=$(curl -s "$line" | wc -w | tr -d '\n')
-    echo -e "$((line_count++))\t${line}\t${http_code}\t${encoding:-Absent}\t${words_count}"
+    http_code=$(curl -s -I -L -w '%{http_code}' -o/dev/null $line)
+    encoding=$(curl -s -I -L -w '%{content_type}' -o/dev/null $line | grep -P -o 'charset=\S+' |cut -d "=" -f2)
+    words_count=$(lynx -dump -nolist $line | wc -w)
+	echo -e "<tr>
+				<td>$((line_count++))</td>
+				<td>{$line}</td>
+				<td>${http_code}</td>
+				<td>${encoding:-Absent}</td>
+				<td>${words_count}<td>
+			</tr>"
 done < $file
+echo -e "</table>
+	</body>
+</html>"
